970535b36eb65aad0b9860b846fb732af1963e40
ashi <500052000@stu.upes.ac.in>
2019-10-22 00:55:41 +0530

    Added more Programs

---
+++ Node.java
@@ -0,0 +1,16 @@
+class Node{
+    private int value;
+    private Node nextNode;
+    public Node getNextNode() {
+        return nextNode;
+    }
+    public void setNextNode(Node nextNode) {
+        this.nextNode = nextNode;
+    }
+    public int getValue() {
+        return value;
+    }
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
